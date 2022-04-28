a68fb8b2a640add3881483b159e8e7893146f1a8
조경진 <cho881020@gmail.com>
2022-04-28 11:53:34 +0900

    Initial commit

---
+++ app/src/main/java/kr/nepp/intent_20220428/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.nepp.intent_20220428
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
