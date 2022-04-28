08e3cb105be634ad2b25200a6dfd61d6cccce6d1
조경진 <cho881020@gmail.com>
2022-04-28 11:58:48 +0900

    새 화면 추가 / 메인 화면 버튼 누르면 이동

--- app/src/main/java/kr/nepp/intent_20220428/MainActivity.kt
+++ app/src/main/java/kr/nepp/intent_20220428/MainActivity.kt
@@ -1,11 +1,21 @@
 package kr.nepp.intent_20220428
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnMove.setOnClickListener {
+
+            val myIntent = Intent( this, OtherActivity::class.java )
+            startActivity(myIntent)
+
+        }
+
     }
 }
